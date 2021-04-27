0f99dd9e5960f31257a6275302a69106e0c30a52
DESKTOP-0KUMNP4\Julian <julianvorbrink@gmail.com>
2021-04-27 16:00:40 +0200

    added modules

--- src/app/pages/new-note/new-note.module.ts
+++ src/app/pages/new-note/new-note.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,9 @@ import { NewNotePage } from './new-note.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    NewNotePageRoutingModule
+    NewNotePageRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   declarations: [NewNotePage]
 })
