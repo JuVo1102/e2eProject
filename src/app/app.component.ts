6366c7806506fe7a9f22759064c7495a1a41ac6a
DESKTOP-0KUMNP4\Julian <julianvorbrink@gmail.com>
2021-04-27 13:25:01 +0200

    firebase installation and initialisation with config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
+import { firebaseEnvironment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    firebase.initializeApp(firebaseEnvironment.firebase);
+  }
 }
